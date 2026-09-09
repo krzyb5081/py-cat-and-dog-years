9ef93a861007bed3d1a74c84677982e023a11e4a
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-09-09 10:02:02 +0200

    Add TypeError test

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pytest
 
 
@@ -17,5 +18,27 @@ from app.main import get_human_age
         (100, 100, [21, 17]),
     ],
 )
-def test_get_human_age(cat_age: int, dog_age: int, expected: list[int, int) -> None:
+def test_get_human_age(
+    cat_age: int,
+    dog_age: int,
+    expected: list[int],
+) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("10", 10),
+        (10, "10"),
+        (None, 5),
+        (5, [1, 2]),
+        ({"age": 3}, 4),
+    ],
+)
+def test_get_human_age_raises_type_error(
+    cat_age: Any,
+    dog_age: Any,
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
